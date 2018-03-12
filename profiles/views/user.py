f22127c525788e87feb769f9c6b0fde1d1b3adb9
diegommarino <diegommarino@gmail.com>
2018-03-12 20:11:28 +0000

    [close #6]_refactor_django_user_model
    - Created a new User model with email for authentication.
    - Needed to reset the database and recreate the migrations. This was only done because I was in the beggining of the project.

--- profiles/views/user.py
+++ profiles/views/user.py
@@ -1,16 +1,16 @@
 from profile_matching.profiles.serializers.user import UserSerializer
 from rest_framework import generics
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
 
 
 class UserList(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, permissions.IsAdminUser)
-    queryset = User.objects.all()
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, permissions.IsAdminUser)
-    queryset = User.objects.all()
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
--- settings.py
+++ settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
+    'profile_matching.core',
     'profile_matching.profiles',
 ]
 
@@ -124,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'core.User'
